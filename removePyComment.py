a9f366accc0e34c00905233a5ce64da3e0a62406
Kimdu0 <dino700072@gmail.com>
2023-10-20 14:38:26 +0900

    Remove python comments and docstrings

---
+++ removePyComment.py
@@ -0,0 +1,20 @@
+import ast
+import astor
+
+def remoceDocstring(node):
+    if isinstance(node, (ast.FunctionDef, ast.ClassDef, ast.AsyncFunctionDef)):
+        node.body = [item for item in node.body if not (isinstance(item, ast.Expr) and hasattr(item, 'value') and isinstance(item.value, ast.Str))]
+
+def process_ast_tree(tree):
+    for node in ast.walk(tree):
+        if isinstance(node, ast.Module):
+            node.body = [item for item in node.body if not (isinstance(item, ast.Expr) and hasattr(item, 'value') and isinstance(item.value, ast.Str))]
+        else:
+            remoceDocstring(node)
+    return astor.to_source(parsed)
+
+if __name__ == "__main__":
+    parsed = ast.parse(open('test.py').read())
+    rem_code = process_ast_tree(parsed)
+    print(rem_code)
+    
